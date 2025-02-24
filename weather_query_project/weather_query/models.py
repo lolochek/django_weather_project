14091eb1267b591927a4d4cf5d7cca58b037539c
lolochek <yankouskaya_yana@mail.ru>
2025-02-25 00:27:57 +0300

    added models and model testing

--- weather_query_project/weather_query/models.py
+++ weather_query_project/weather_query/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Query(models.Model):
+    city_name = models.TextField(max_length=100)
+    timestamp = models.DateTimeField(auto_now=True)
+    weather_details = models.JSONField()
+
--- weather_query_project/weather_query/tests.py
+++ weather_query_project/weather_query/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Query
 
 # Create your tests here.
+
+class ModelTesting(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.q1 = Query.objects.create(city_name="Minsk",
+                                  weather_details={'weather': [{'id': 800, 'main': 'Clear', 'description': 'clear sky'}]})
+    def test_query_content(self):
+        self.assertEqual(self.q1.city_name, "Minsk")
+        self.assertIsNotNone(self.q1.timestamp)
+        self.assertEqual(self.q1.weather_details['weather'][0]['main'], 'Clear')
+        self.assertEqual(self.q1.weather_details['weather'][0]['description'], 'clear sky')
